04ae230ab51f4138e0e100b2920b36791283c0b7
Yoan Durand <yoan@follow.fr>
2019-03-25 14:16:11 +0100

    are the same

--- src/main.rs
+++ src/main.rs
@@ -15,11 +15,28 @@ fn xo(string: &'static str) -> bool {
     return count_x == count_o;
 }
 
+fn comp(a: Vec<i64>, b: Vec<i64>) -> bool {
+    let mut res = false;
+    for item in a {
+        let squared = item * item;
+        res = b.iter().position(|&y| y == squared) != None;
+
+    }
+
+    res
+}
+
+fn testing(a: Vec<i64>, b: Vec<i64>, exp: bool) -> () {
+    assert_eq!(comp(a, b), exp)
+}
+
 fn main() {
     assert_eq!(xo("xo"), true);
-    assert_eq!(xo("Xo"), true);
-    assert_eq!(xo("xxOo"), true);
-    assert_eq!(xo("xxxm"), false);
-    assert_eq!(xo("Oo"), false);
-    assert_eq!(xo("ooom"), false);
+
+    let a1 = vec![121, 144, 19, 161, 19, 144, 19, 11];
+    let a2 = vec![11*11, 121*121, 144*144, 19*19, 161*161, 19*19, 144*144, 19*19];
+    testing(a1, a2, true);
+    let a1 = vec![121, 144, 19, 161, 19, 144, 19, 11];
+    let a2 = vec![11*21, 121*121, 144*144, 19*19, 161*161, 19*19, 144*144, 19*19];
+    testing(a1, a2, false);
 }
